defc435e102821e3238926a5d42cc33819d915fc
valeria campilongo <cvaleriac@hotmail.it>
2019-03-19 13:16:44 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-root 'application#hello'
-get '/login' => 'sessions#new'
-post '/login' => 'sessions#create'
-post '/logout' => 'sessions#destroy'# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/show' => 'secrets#show'
+   post '/create' => 'sessions#create'
+   get '/new' => 'sessions#new'
+   get '/home' => 'sessions#home'
+   post '/destroy' => 'sessions#destroy'
 end
